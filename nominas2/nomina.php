bce2a70d480233844c682bf0c911742de38d7750
Eric Romero <45190598+rore98@users.noreply.github.com>
2019-05-25 10:28:04 -0500

    Add files via upload
    Diseño parte 1.

---
+++ nominas2/nomina.php
@@ -0,0 +1,15 @@
+<?php
+@include('nav.php')
+?>
+<title>NOMINA</title>
+ <main class="main">
+            <div class="container2">
+                <h1>Nomina</h1>
+            </div>
+</main>
+</form>
+<?php
+@include('footer.php')
+?>
+    </body>
+</html>
---
+++ nominas2/reporte.php
@@ -0,0 +1,15 @@
+<?php
+@include('nav.php')
+?>
+<title>REPORTE</title>
+ <main class="main">
+            <div class="container2">
+                <h1>Reporte de Nomina</h1>
+            </div>
+</main>
+</form>
+<?php
+@include('footer.php')
+?>
+    </body>
+</html>
